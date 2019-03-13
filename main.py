8fd0caf00277d137efd205cc6fc70168f111efe1
YezhenWang <309426989@qq.com>
2019-03-13 18:08:52 +0800

    18:08 03/13/19 in harbin

--- main.py
+++ main.py
@@ -13,12 +13,14 @@ import sys
 import torchvision.transforms as transforms
 MAE = 10240000
 MSE = 10240000
+RATE = 10000000
 SHANGHAITECH = "B"
+# %matplotlib inline
 # data_load
 img_dir = "/home/zzn/part_" + SHANGHAITECH + "_final/train_data/images"
 gt_dir = "/home/zzn/part_" + SHANGHAITECH + "_final/train_data/gt_map"
 
-dataset = DatasetConstructor(img_dir, gt_dir, 400, 20)
+dataset = DatasetConstructor(img_dir, gt_dir, 400, 50)
 train_loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=4)
 eval_loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=1)
 # obtain the gpu device
@@ -27,16 +29,16 @@ cuda_device = torch.device("cuda")
 
 # model construct
 net = SANet().to(cuda_device)
-
+# net = torch.load("/home/zzn/PycharmProjects/SANet_pytoch/checkpoints/model_mse_b_0312.pkl").to(cuda_device)
 # set optimizer and estimator
 criterion = SANetLoss(1).to(cuda_device)
-optimizer = torch.optim.Adam(net.parameters(), 1e-5, weight_decay=5*1e-4)
+optimizer = torch.optim.Adam(net.parameters(), 1e-6)
 ae_batch = AEBatch().to(cuda_device)
 se_batch = SEBatch().to(cuda_device)
-for epoch_index in range(500):
+
+step = 0
+for epoch_index in range(10000):
     dataset = dataset.train_model().shuffle()
-    # train
-    step = 0
     for train_img_index, train_img, train_gt, data_ptc in train_loader:
         # eval per 100 batch
         if step % 100 == 0:
@@ -45,40 +47,68 @@ for epoch_index in range(500):
             loss_ = []
             MAE_ = []
             MSE_ = []
+            difference_rates = []
 
             rand_number = random.randint(0, 19)
             counter = 0
 
             for eval_img_index, eval_img, eval_gt, eval_data_ptc in eval_loader:
 
+                image_shape = eval_img.shape
+                patch_height = int(image_shape[3])
+                patch_width = int(image_shape[4])
                 # B
-                eval_x = eval_img.view(-1, 3, 768, 1024).cuda()
-                eval_y = eval_gt.view(-1, 1, 768, 1024).cuda()
-                # A
-                #                     eval_x = eval_img.cuda()
-                #                     eval_y = eval_gt.cuda()
-                eval_prediction = net(eval_x)
+                eval_x = eval_img.view(49, 3, patch_height, patch_width)
+                eval_y = eval_gt.view(1, 1, patch_height * 4, patch_width * 4).cuda()
+                prediction_map = torch.zeros(1, 1, patch_height * 4, patch_width * 4).cuda()
+                for i in range(7):
+                    for j in range(7):
+                        eval_x_sample = eval_x[i * 7 + j:i * 7 + j + 1].cuda()
+                        eval_prediction = net(eval_x_sample)
+                        start_h = int(patch_height / 4)
+                        start_w = int(patch_width / 4)
+                        valid_h = int(patch_height / 2)
+                        valid_w = int(patch_width / 2)
+                        h_pred = 3 * int(patch_height / 4) + 2 * int(patch_height / 4) * (i - 1)
+                        w_pred = 3 * int(patch_width / 4) + 2 * int(patch_width / 4) * (j - 1)
+                        if i == 0:
+                            valid_h = int((3 * patch_height) / 4)
+                            start_h = 0
+                            h_pred = 0
+                        elif i == 6:
+                            valid_h = int((3 * patch_height) / 4)
+
+                        if j == 0:
+                            valid_w = int((3 * patch_width) / 4)
+                            start_w = 0
+                            w_pred = 0
+                        elif j == 6:
+                            valid_w = int((3 * patch_width) / 4)
+
+                        prediction_map[:, :, h_pred:h_pred + valid_h, w_pred:w_pred + valid_w] += eval_prediction[:, :,
+                                                                                                  start_h:start_h + valid_h,
+                                                                                                  start_w:start_w + valid_w]
                 # That’s because numpy doesn’t support CUDA,
                 # so there’s no way to make it use GPU memory without a copy to CPU first.
                 # Remember that .numpy() doesn’t do any copy,
                 # but returns an array that uses the same memory as the tensor
-                eval_loss = criterion(eval_prediction, eval_y).data.cpu().numpy()
-                batch_ae = ae_batch(eval_prediction, eval_y).data.cpu().numpy()
-                batch_se = se_batch(eval_prediction, eval_y).data.cpu().numpy()
+                eval_loss = criterion(prediction_map, eval_y).data.cpu().numpy()
+                batch_ae = ae_batch(prediction_map, eval_y).data.cpu().numpy()
+                batch_se = se_batch(prediction_map, eval_y).data.cpu().numpy()
 
+                validate_pred_map = np.squeeze(prediction_map.permute(0, 2, 3, 1).data.cpu().numpy())
+                validate_gt_map = np.squeeze(eval_y.permute(0, 2, 3, 1).data.cpu().numpy())
+                gt_counts = np.sum(validate_gt_map)
+                pred_counts = np.sum(validate_pred_map)
                 # random show 1 sample
-                if rand_number == counter:
+                if rand_number == counter and step % 2000 == 0:
                     origin_image = Image.open("/home/zzn/part_" + SHANGHAITECH + "_final/train_data/images/IMG_" + str(
                         eval_img_index.numpy()[0]) + ".jpg")
-                    validate_pred_map = np.squeeze(eval_prediction.permute(0, 2, 3, 1).data.cpu().numpy())
-                    validate_gt_map = np.squeeze(eval_y.permute(0, 2, 3, 1).data.cpu().numpy())
-
                     show(origin_image, validate_gt_map, validate_pred_map, eval_img_index.numpy()[0])
-                    gt_counts = np.sum(validate_gt_map)
-                    pred_counts = np.sum(validate_pred_map)
                     sys.stdout.write(
                         'The gt counts of the above sample:{}, and the pred counts:{}\n'.format(gt_counts, pred_counts))
 
+                difference_rates.append(np.abs(gt_counts - pred_counts) / gt_counts)
                 loss_.append(eval_loss)
                 MAE_.append(batch_ae)
                 MSE_.append(batch_se)
@@ -92,43 +122,41 @@ for epoch_index in range(500):
             validate_loss = np.mean(loss_)
             validate_MAE = np.mean(MAE_)
             validate_RMSE = np.sqrt(np.mean(MSE_))
+            validate_rate = np.mean(difference_rates)
 
             sys.stdout.write(
-                'In step {}, epoch {}, with loss {}, MAE = {}, MSE = {}\n'.format(step, epoch_index + 1, validate_loss,
-                                                                                  validate_MAE, validate_RMSE))
+                'In step {}, epoch {}, with loss {}, rate = {}, MAE = {}, MSE = {}\n'.format(step, epoch_index + 1,
+                                                                                             validate_loss,
+                                                                                             validate_rate,
+                                                                                             validate_MAE,
+                                                                                             validate_RMSE))
             sys.stdout.flush()
+
+            if RATE > validate_rate:
+                RATE = validate_rate
+                torch.save(net, "/home/zzn/PycharmProjects/SANet_pytoch/checkpoints/model_1_rate_b.pkl")
 
             # save model
             if MAE > validate_MAE:
                 MAE = validate_MAE
-                torch.save(net, "/home/zzn/PycharmProjects/SANet_pytoch/checkpoints/model_mae_b.pkl")
+                torch.save(net, "/home/zzn/PycharmProjects/SANet_pytoch/checkpoints/model_1_mae_b.pkl")
 
             # save model
             if MSE > validate_RMSE:
                 MSE = validate_RMSE
-                torch.save(net, "/home/zzn/PycharmProjects/SANet_pytoch/checkpoints/model_mse_b.pkl")
+                torch.save(net, "/home/zzn/PycharmProjects/SANet_pytoch/checkpoints/model_1_mse_b.pkl")
 
-            torch.save(net, "/home/zzn/PycharmProjects/SANet_pytoch/checkpoints/model_in_time.pkl")
+            torch.save(net, "/home/zzn/PycharmProjects/SANet_pytoch/checkpoints/model_1_in_time.pkl")
 
             # return train model
-            net.train()
-            dataset = dataset.train_model()
 
         net.train()
+        dataset = dataset.train_model()
         optimizer.zero_grad()
         # B
-        x = train_img.view(-1, 3, 384, 512).cuda()
-        y = train_gt.view(-1, 1, 384, 512).cuda()
+        x = train_img.cuda()
+        y = train_gt.cuda()
 
-        # A
-        #       x = train_img.cuda()
-        #       y = train_gt.cuda()
-        #       figure, (input_picture, gt_picture) = plt.subplots(1, 2, figsize=(20, 4))
-        #       input_picture.imshow(train_img[0].view(3, 384, 512).permute(1, 2, 0).numpy())
-        #       input_picture.set_title("origin")
-        #       gt_picture.imshow(train_gt[0].view(384, 512).numpy(), cmap=plt.cm.jet)
-        #       gt_picture.set_title("gt")
-        #       plt.show()
         prediction = net(x)
         loss = criterion(prediction, y)
         loss.backward()
--- predict.py
+++ predict.py
@@ -11,62 +11,86 @@ import metrics
 from PIL import Image
 import time
 SHANGHAITECH = "B"
+# %matplotlib inline
 # obtain the gpu device
 assert torch.cuda.is_available()
 cuda_device = torch.device("cuda")  # device object representing GPU
-
 # data_load
 img_dir = "/home/zzn/part_" + SHANGHAITECH + "_final/test_data/images"
 gt_dir = "/home/zzn/part_" + SHANGHAITECH + "_final/test_data/gt_map"
-transform_a = transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-            ])
-gt_transform_a =  transforms.ToTensor()
-
-dataset = DatasetConstructor(img_dir, gt_dir, 316, 316, transform_a, gt_transform_a, False)
+dataset = DatasetConstructor(img_dir, gt_dir, 316, 316, False)
 test_loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=1)
-
 mae_metrics = []
 mse_metrics = []
-net = torch.load("/home/zzn/PycharmProjects/SANet_pytoch/checkpoints/model_mae_b.pkl").to(cuda_device)
+net = torch.load("/home/zzn/PycharmProjects/SANet_pytoch/checkpoints/model_rate_b.pkl").to(cuda_device)
 net.eval()
 
 
 ae_batch = metrics.AEBatch().to(cuda_device)
 se_batch = metrics.SEBatch().to(cuda_device)
-
 for real_index, test_img, test_gt, test_time_cost in test_loader:
-    if True:
-        predict_x = test_img.cuda()
-        predict_gt = test_gt.cuda()
-    predict_predict_map = net(predict_x)
-    predict_gt_map = predict_gt
-    batch_ae = ae_batch(predict_predict_map, predict_gt_map).data.cpu().numpy()
-    batch_se = se_batch(predict_predict_map, predict_gt_map).data.cpu().numpy()
+    image_shape = test_img.shape
+    patch_height = int(image_shape[3])
+    patch_width = int(image_shape[4])
+    # B
+    eval_x = test_img.view(49, 3, patch_height, patch_width)
+    eval_y = test_gt.view(1, 1, patch_height * 4, patch_width * 4).cuda()
+    prediction_map = torch.zeros(1, 1, patch_height * 4, patch_width * 4).cuda()
+    for i in range(7):
+        for j in range(7):
+            eval_x_sample = eval_x[i * 7 + j:i * 7 + j + 1].cuda()
+            eval_y_sample = eval_y[i * 7 + j:i * 7 + j + 1].cuda()
+            eval_prediction = net(eval_x_sample)
+            start_h = int(patch_height / 4)
+            start_w = int(patch_width / 4)
+            valid_h = int(patch_height / 2)
+            valid_w = int(patch_width / 2)
+            h_pred = 3 * int(patch_height / 4) + 2 * int(patch_height / 4) * (i - 1)
+            w_pred = 3 * int(patch_width / 4) + 2 * int(patch_width / 4) * (j - 1)
+            if i == 0:
+                valid_h = int((3 * patch_height) / 4)
+                start_h = 0
+                h_pred = 0
+            elif i == 6:
+                valid_h = int((3 * patch_height) / 4)
+
+            if j == 0:
+                valid_w = int((3 * patch_width) / 4)
+                start_w = 0
+                w_pred = 0
+            elif j == 6:
+                valid_w = int((3 * patch_width) / 4)
+
+            prediction_map[:, :, h_pred:h_pred + valid_h, w_pred:w_pred + valid_w] += eval_prediction[:, :,
+                                                                                      start_h:start_h + valid_h,
+                                                                                      start_w:start_w + valid_w]
+
+    batch_ae = ae_batch(prediction_map, eval_y).data.cpu().numpy()
+    batch_se = se_batch(prediction_map, eval_y).data.cpu().numpy()
     mae_metrics.append(batch_ae)
     mse_metrics.append(batch_se)
     # to numpy
-    numpy_predict_map = predict_predict_map.permute(0, 2, 3, 1).data.cpu().numpy()
-    numpy_gt_map = predict_gt_map.permute(0, 2, 3, 1).data.cpu().numpy()
+    numpy_predict_map = prediction_map.permute(0, 2, 3, 1).data.cpu().numpy()
+    numpy_gt_map = eval_y.permute(0, 2, 3, 1).data.cpu().numpy()
 
     # show current prediction
     figure, (origin, dm_gt, dm_pred) = plt.subplots(1, 3, figsize=(20, 4))
     origin.imshow(Image.open("/home/zzn/part_B_final/test_data/images/IMG_" + str(real_index.numpy()[0]) + ".jpg"))
     origin.set_title('Origin Image')
     dm_gt.imshow(np.squeeze(numpy_gt_map), cmap=plt.cm.jet)
-    dm_gt.set_title('ground_truth')
+    dm_gt.set_title('ground_truth_1')
+
     dm_pred.imshow(np.squeeze(numpy_predict_map), cmap=plt.cm.jet)
     dm_pred.set_title('prediction')
-    plt.suptitle('The ' + str(real_index.numpy()[0]) + 'st images\'prediction')
+
+    plt.suptitle('The ' + str(real_index.numpy()[0]) + 'th images\'prediction')
     plt.show()
     sys.stdout.write('The grount truth crowd number is:{}, and the predicting number is:{}'.format(np.sum(numpy_gt_map),
                                                                                                    np.sum(
                                                                                                        numpy_predict_map)))
     sys.stdout.flush()
-
-mae_metrics = np.reshape(mae_metrics, [-1])
-mse_metrics = np.reshape(mse_metrics, [-1])
-MAE = np.mean(mae_metrics)
-MSE = np.sqrt(np.mean(mse_metrics))
-print('MAE:', MAE, 'MSE:', MSE)
+    mae_metrics = np.reshape(mae_metrics, [-1])
+    mse_metrics = np.reshape(mse_metrics, [-1])
+    MAE = np.mean(mae_metrics)
+    MSE = np.sqrt(np.mean(mse_metrics))
+    print('MAE:', MAE, 'MSE:', MSE)
